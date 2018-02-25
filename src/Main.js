8d0837c4782237a763c685f2cbea4cf106a321ed
Joshua D <joshdecosta@gmail.com>
2018-02-25 18:42:39 -0500

    removes url and creates slightly more secure environment variables.
    I had originally left in a bunch of testing stuff that was wide open
    for mayhem. But after learning a bit about react and how to componentize
    things, I've since stepped up my game a bit with new stuff.

--- src/Main.js
+++ src/Main.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
 import headerImage from './banner.jpg';
 import './Main.css';
-
-
+import env from './env.json';
 
 class Menu extends Component {
     render() {
         return (
             <div className="Menu">
                 <nav>
-                <ul className="nav-menu-list">
-                    <li className="nav"><Link to='/'>About</Link></li>
-                    <li className="nav"><Link to='/location'>Directions</Link></li>
-                </ul>
+                    <ul>
+                        <li><Link to='/'>About</Link></li>
+                        <li><Link to='/location'>Directions</Link></li>
+                    </ul>
                 </nav>
             </div>
         );
@@ -33,7 +32,7 @@ class Header extends Component {
     }
 }
 
-class Home extends Component {
+class SubmissionForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -44,7 +43,8 @@ class Home extends Component {
             Beer: false,
             Wine: false,
             Selzter: false,
-            Soda: false
+            Soda: false,
+            travelPool: ''
         };
     
         this.handleChange = this.handleChange.bind(this);
@@ -65,18 +65,15 @@ class Home extends Component {
     
       handleSubmit(event) {
         event.preventDefault();
-        // const dataObj = new FormData(event.target);
-        var url = 'https://script.google.com/macros/s/AKfycbwhiT80X_X1WtxbywuRhLWA_wEmdeRwz7relpq6VcxxlXoBgKE/exec';
-        var object = "?";
+        var url = env['url'];
+        var params = "?";
 
         // Display the key/value pairs
         for(var key in this.state) {
-            object += encodeURIComponent(key) + "=" +  encodeURIComponent(this.state[key]);
-            object += "&";
+            params += encodeURIComponent(key) + "=" +  encodeURIComponent(this.state[key]);
+            params += "&";
         }
-        console.log(object);
-        // console.log(url+object);
-        url += object.substr(0, object.length-1);
+        url += params.substr(0, params.length-1);
 
         fetch(url, {
           method: "GET",
@@ -86,66 +83,104 @@ class Home extends Component {
                 alert("Something went wrong, feel free to email josh: joshdecosta@gmail.com");
                 throw Error(res.statusText);
             } else if (res.ok) {
-                alert("thanks for responding!");
+                alert("thanks for responding! your form submission was successful");
             }
             console.log(res.data);
         });
       }
+      render () {
+          return (
+            <form onSubmit={this.handleSubmit}>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="Name" required pattern="[A-Za-z ]+" value={this.state.Name} onChange={this.handleChange}/>
+            </div>
+            {this.state.plusOne && <input type="checkbox" id="plusOne" name="plusOne"/>}
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="Email" required pattern="[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z]+" value={this.state.Email} onChange={this.handleChange} />
+            </div>
+            <div>
+                <fieldset>
+                    <legend>Will you be attending?</legend>
+                    <label htmlFor="Yes">Yes:</label>
+                    <input type="radio" name="Attending" id="Yes" value="Yes" onChange={this.handleChange} checked={this.state.Attending === "Yes"} />
+                    <label htmlFor="No">No:</label>
+                    <input type="radio" name="Attending" id="No" value="No" onChange={this.handleChange} checked={this.state.Attending === "No"} />                        
+                </fieldset>
+            </div>
+            <div>
+                <fieldset>
+                    <legend>Vegetarian or meat?</legend>
+                    <label htmlFor="Veg">Vegetarian:</label>
+                    <input type="radio" name="Food" id="Veg" value="Veg" onChange={this.handleChange} checked={this.state.Food === "Veg"} />                        
+                    <label htmlFor="Meat">Meat:</label>
+                    <input type="radio" name="Food" id="Meat" value="Meat" onChange={this.handleChange} checked={this.state.Food === "Meat"} />                        
+                </fieldset>
+            </div>
+            <div>
+                <fieldset>
+                    <legend>What will you be drinking?</legend>
+                    <label htmlFor="Beer">Beer</label>
+                    <input type="checkbox" name="Beer" id="Beer" onChange={this.handleChange} checked={this.state.Beer === true} />
+                    <label htmlFor="Wine">Wine</label>
+                    <input type="checkbox" name="Wine" id="Wine" onChange={this.handleChange} checked={this.state.Wine === true} />
+                    <label htmlFor="Selzter">Selzter</label>
+                    <input type="checkbox" name="Selzter" id="Selzter" onChange={this.handleChange} checked={this.state.Selzter === true} />
+                    <label htmlFor="Soda">Soda</label>
+                    <input type="checkbox" name="Soda" id="Soda" onChange={this.handleChange} checked={this.state.Soda === true} />
+                </fieldset>                                                
+            </div>
+            <div>
+                <fieldset>
+                    <legend>Travel pool</legend>
+                <label htmlFor="contribute">I can help</label>
+                <input type="radio" name="travelPool" id="contribute" value="contribute" onChange={this.handleChange} checked={this.state.travelPool === "contribute"} />
+                <label htmlFor="needsHelp">I need help</label>
+                <input type="radio" name="travelPool" id="needsHelp" value="needsHelp" onChange={this.handleChange} checked={this.state.travelPool === "needsHelp"} /> 
+                </fieldset>
+            </div>
+            <div className="button">
+                <button type="submit">Send it</button>
+            </div>
+        </form>
+        )
+      }
+
     
-    
+
+
+}
+
+class Home extends Component {
 
     render() {
         return (
             <div>
-                <h1>Family Gathering Party</h1>
-                <h2>We will be having a party, please fill out the form below to tell us if 
-                    You'll be attending 
-                </h2>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" id="name" name="Name" required pattern="[A-Za-z ]+" value={this.state.Name} onChange={this.handleChange}/>
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="Email" required value={this.state.Email} onChange={this.handleChange} />
-                    </div>
-                    <div>
-                        <fieldset>
-                            <legend>Will you be attending?</legend>
-                            <label htmlFor="Yes">Yes:</label>
-                            <input type="radio" name="Attending" id="Yes" value="Yes" onChange={this.handleChange} checked={this.state.Attending === "Yes"} />
-                            <label htmlFor="No">No:</label>
-                            <input type="radio" name="Attending" id="No" value="No" onChange={this.handleChange} checked={this.state.Attending === "No"} />                        
-                        </fieldset>
-                    </div>
-                    <div>
-                        <fieldset>
-                            <legend>Vegetarian or meat?</legend>
-                            <label htmlFor="Veg">Vegetarian:</label>
-                            <input type="radio" name="Food" id="Veg" value="Veg" onChange={this.handleChange} checked={this.state.Food === "Veg"} />                        
-                            <label htmlFor="Meat">Meat:</label>
-                            <input type="radio" name="Food" id="Meat" value="Meat" onChange={this.handleChange} checked={this.state.Food === "Meat"} />                        
-                        </fieldset>
-                    </div>
-                    <div>
-                        <fieldset>
-                            <legend>What will you be drinking?</legend>
-                            <label htmlFor="Beer">Beer</label>
-                            <input type="checkbox" name="Beer" id="Beer" onChange={this.handleChange} checked={this.state.Beer === true} />
-                            <label htmlFor="Wine">Wine</label>
-                            <input type="checkbox" name="Wine" id="Wine" onChange={this.handleChange} checked={this.state.Wine === true} />
-                            <label htmlFor="Selzter">Selzter</label>
-                            <input type="checkbox" name="Selzter" id="Selzter" onChange={this.handleChange} checked={this.state.Selzter === true} />
-                            <label htmlFor="Soda">Soda</label>
-                            <input type="checkbox" name="Soda" id="Soda" onChange={this.handleChange} checked={this.state.Soda === true} />
-                        </fieldset>                                                
-                    </div>
-                    <div className="button">
-                        <button type="submit">Send it</button>
-                        {/* <button>Submit</button> */}
-                    </div>
-                </form>
+                <h1>We're having a party!</h1>
+                <h3>When: Saturday, June 16th, 2018</h3>
+                <h3>Time: 4pm to 9:30pm</h3>
+                <h3>Where: Newton Community Farm, 303 Nahanton Street, Newton, MA 02459</h3>
+                <p> 
+                    So you've probably been wondering when this event would occur and we've finally
+                    gotten all the details together for you. This is an event to bring family and friends
+                    together under the same roof and celebrate our lives. We appreciate all that you've 
+                    brought to ours over the years and thought everyone should meet, and now seems like
+                    as good a time as ever. 
+                </p>
+                <p>
+                    Below is a form to let us know if you're coming or not.
+                We wanted to host this event for a simple reason: bring together people we love most. 
+                We know from our own lives that traveling can be a big financial burden — 
+                but we want to find a way to remove that barrier for everyone we invited through a travel pool. 
+                Please consider <a href="https://paypal.me/pools/c/82cskaxLW6">contributing a gift</a> to help others travel, or reach out if financial assistance 
+                would mean the difference for you attending. Any money that isn’t used from the pool will be 
+                donated to The Haley House. Thank for you helping to make this party everything we ever dreamed of. 
+                If you submit the form below and a pop up doesn't appear, please try again, or shut off your ad-blocker plugins. 
+                Those can cause issues with the form submission. If it still doesn't work then email <a href="mailto:joshdecosta@gmail.com">Josh</a> and 
+                he can sort it out.
+                </p>
+                <SubmissionForm/>
             </div>
         );
     }
@@ -177,7 +212,7 @@ class Content extends Component {
 class Main extends Component {
     render() {
         return (
-            <div>
+            <div className="Main">
                 <Header />
                 <Content />
             </div>
