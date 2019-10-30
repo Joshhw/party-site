a96a3340f7233517ac5521d259acc69bf52f7ec4
Joshua Decosta <joshdecosta@gmail.com>
2019-10-29 21:16:34 -0400

    adds routing functionality

--- jessandjosh/src/app/App.tsx
+++ jessandjosh/src/app/App.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
-import Body from '../body/Body';
+import style from 'styled-components';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '../home/Home';
+const ContentWrapper = style.div`
+  position: relative;
+  min-height: 100vh;
+`;
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Header />
-      <Body />
-      <Footer />
+      <Router>
+        <Header />
+        <ContentWrapper>
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </ContentWrapper>
+        <Footer />
+      </Router>
     </div>
   );
 };
